3670df5c984a24a8848df41c9f9993be2d82bc81
Iska020 <IskandarMustafin@gmail.com>
2022-02-17 15:42:07 +0300

    Some little changes in BaseTest

--- src/test/java/ru/demo/mirapolis/tests/BaseTest.java
+++ src/test/java/ru/demo/mirapolis/tests/BaseTest.java
@@ -7,17 +7,13 @@ import org.junit.Before;
 
 public abstract class BaseTest {
 
+    @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Iskan\\Documents\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Iskan\\IdeaProjects\\chromedriver.exe");
         System.setProperty("selenide.browser", "Chrome");
         Configuration.driverManagerEnabled = true;
         Configuration.browserSize = "1600x900";
         Configuration.headless = false;
-    }
-
-    @Before
-    public void init() {
-        setUp();
     }
 
     @After
